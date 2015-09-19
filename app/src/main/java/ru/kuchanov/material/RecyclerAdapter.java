f4ad1082a0ac0b032cdb0d7c1a4c44980e4f55c1
mohaxspb <mohax.spb@gmail.com>
2015-09-20 02:39:34 +0300

    Add theme to manifest; Add materialRippleLayout lib; Return simple layout for recyclerView's item;

--- app/src/main/java/ru/kuchanov/material/RecyclerAdapter.java
+++ app/src/main/java/ru/kuchanov/material/RecyclerAdapter.java
@@ -6,34 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * Created by Юрий on 17.09.2015.
- */
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>
 {
 
     private String[] mDataset;
 
-    // Конструктор
     public RecyclerAdapter(String[] dataset)
     {
         mDataset = dataset;
     }
 
-//    @Override
-//    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-//    {
-//        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
-//        ViewHolder vh = new ViewHolder(v);
-//        return vh;
-//    }
-@Override
-public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-{
-    View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.article_card_image_right, parent, false);
-    ViewHolder vh = new ViewHolder(v);
-    return vh;
-}
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position)
@@ -54,7 +43,7 @@ public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewT
         public ViewHolder(View v)
         {
             super(v);
-            mTextView = (TextView) v.findViewById(R.id.art_card_title_tv);
+            mTextView = (TextView) v.findViewById(R.id.title);
         }
     }
 }
