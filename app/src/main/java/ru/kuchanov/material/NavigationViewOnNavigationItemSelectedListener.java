41c12ab6210a66934f502b3ff323d668983d1b0f
mohaxspb <mohax.spb@gmail.com>
2015-09-20 03:18:18 +0300

    Return transparent statusBar on api>19

--- app/src/main/java/ru/kuchanov/material/NavigationViewOnNavigationItemSelectedListener.java
+++ app/src/main/java/ru/kuchanov/material/NavigationViewOnNavigationItemSelectedListener.java
@@ -1,11 +1,14 @@
 package ru.kuchanov.material;
 
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NavigationViewOnNavigationItemSelectedListener implements NavigationView.OnNavigationItemSelectedListener
 {
@@ -25,25 +28,33 @@ public class NavigationViewOnNavigationItemSelectedListener implements Navigatio
     {
         int checkedDrawerItemId = menuItem.getItemId();
 
+        int colorId;
+        int[] attrs = new int[]{android.R.attr.colorPrimaryDark};
+        TypedArray ta = this.pager.getContext().obtainStyledAttributes(attrs);
+        colorId = ta.getColor(0, Color.GRAY);
+        ta.recycle();
+
         Snackbar snackbar;
+        String snackBarMsg = "";
         switch (menuItem.getItemId())
         {
             case R.id.tab_1:
-                snackbar = Snackbar.make(drawerLayout, "tab_1", Snackbar.LENGTH_SHORT);
-                snackbar.show();
+                snackBarMsg = "tab_1";
                 pager.setCurrentItem(0, true);
                 break;
             case R.id.tab_2:
-                snackbar = Snackbar.make(drawerLayout, "tab_2", Snackbar.LENGTH_SHORT);
-                snackbar.show();
+                snackBarMsg = "tab_2";
                 pager.setCurrentItem(1, true);
                 break;
             case R.id.tab_3:
-                snackbar = Snackbar.make(drawerLayout, "tab_3", Snackbar.LENGTH_SHORT);
-                snackbar.show();
+                snackBarMsg = "tab_3";
                 pager.setCurrentItem(2, true);
                 break;
         }
+        snackbar = Snackbar.make(pager, snackBarMsg, Snackbar.LENGTH_SHORT);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(colorId);
+        snackbar.show();
 
         callbackDrawerSelected.updateNavigationViewState(checkedDrawerItemId);
 
